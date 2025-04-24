7d342d90bf6351640a4d42b7baa13c8a82dba331
Wdboyes13 <willdev2025@outlook.com>
2025-04-23 18:54:19 -0700

    Version v1.0.4

--- app-pkg.sh
+++ app-pkg.sh
@@ -1,7 +1,11 @@
 # mac packaging
-cp ~/coding/grl/app/build/javafx-libs/*mac.jar ~/coding/grl/OSpkg/morning.app/Contents/Resources/lib/intel/
-cp ~/coding/grl/app/build/javafx-libs/*mac-aarch64.jar ~/coding/grl/OSpkg/morning.app/Contents/Resources/lib/aarch64/
-cp ~/coding/grl/app/build/libs/app-all.jar ~/coding/grl/OSpkg/morning.app/Contents/Resources/
-chmod +x ~/coding/grl/OSpkg/morning.app/Contents/MacOS/morning
-codesign --deep --force --verbose --sign "Apple Development: wdboyes@icloud.com (5SFDQ6365C)" ~/coding/grl/OSpkg/morning.app
-zip -r ~/coding/grl/OSpkg/morning.app.zip ~/coding/grl/OSpkg/morning.app/
+cp ~/coding/grl/app/build/javafx-libs/*mac.jar ~/coding/grl/OSpkg/lib/mac-intel/
+cp ~/coding/grl/app/build/javafx-libs/*mac-aarch64.jar ~/coding/grl/OSpkg/lib/mac-aarch64/
+cp ~/coding/grl/app/build/libs/app-all.jar ~/coding/grl/OSpkg/lib/mac-aarch64/
+cp ~/coding/grl/app/build/libs/app-all.jar ~/coding/grl/OSpkg/lib/mac-intel/
+rm -rf ~/coding/grl/app/Morning-arm.app
+rm -rf ~/coding/grl/app/Morning-x64.app
+jpackage --type app-image --input ~/coding/grl/OSpkg/lib/mac-aarch64 --name Morning-arm --main-jar app-all.jar --main-class org.example.App --java-options "--module-path $MOD_PATH --add-modules javafx.controls,javafx.fxml"
+jpackage --type app-image --input ~/coding/grl/OSpkg/lib/mac-intel --name Morning-x64 --main-jar app-all.jar --main-class org.example.App --java-options "--module-path $MOD_PATH --add-modules javafx.controls,javafx.fxml"
+zip -r ~/coding/grl/dist/Morning-arm.app.zip ~/coding/grl/app/Morning-arm.app/ 
+zip -r ~/coding/grl/dist/Morning-x64.app.zip ~/coding/grl/app/Morning-x64.app/ 
