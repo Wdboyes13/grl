d0d1ba7f9dc6cc53f190c87fc539b0d3be95e782
Wdboyes13 <willdev2025@outlook.com>
2025-04-23 17:45:46 -0700

    FINALLY packaged the .app properly (sorry about the false version tags)

--- app-pkg.sh
+++ app-pkg.sh
@@ -4,9 +4,4 @@ cp ~/coding/grl/app/build/javafx-libs/*mac-aarch64.jar ~/coding/grl/OSpkg/mornin
 cp ~/coding/grl/app/build/libs/app-all.jar ~/coding/grl/OSpkg/morning.app/Contents/Resources/
 chmod +x ~/coding/grl/OSpkg/morning.app/Contents/MacOS/morning
 codesign --deep --force --verbose --sign "Apple Development: wdboyes@icloud.com (5SFDQ6365C)" ~/coding/grl/OSpkg/morning.app
-
-# windows packaging
-cp -r ~/coding/jre21-win/* ~/coding/grl/OSpkg/win/jre/
-cp ~/coding/grl/app/build/javafx-libs/*win.jar ~/coding/grl/OSpkg/win/lib/
-cp ~/coding/grl/app/build/libs/app-all.jar ~/coding/grl/OSpkg/win/
-java -jar /Users/william/launch4j/launch4j.jar ~/coding/grl/OSpkg/win/config.xml
+zip -r ~/coding/grl/OSpkg/morning.app.zip ~/coding/grl/OSpkg/morning.app/
