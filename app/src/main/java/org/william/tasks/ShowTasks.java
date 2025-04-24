d722c680119c6f70ac735bd6788bca5e757d5006
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 07:32:05 -0700

    Version v1.0.4

--- app/src/main/java/org/william/tasks/ShowTasks.java
+++ app/src/main/java/org/william/tasks/ShowTasks.java
@@ -16,6 +16,7 @@ public class ShowTasks {
         root.getChildren().add(back);
         Text label = new Text(Menu.text);
         label.setTranslateX(100);
+        root.getChildren().add(label);
         List<CheckBox> checkBoxes;
 
         if (checkboxCache.containsKey(taskListName)){
