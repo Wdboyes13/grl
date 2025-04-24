eec34ac927c6f198d73a26a9e2b07365a9877dae
Wdboyes13 <willdev2025@outlook.com>
2025-04-23 17:22:57 -0700

    fixed jre

--- app-pkg.sh
+++ app-pkg.sh
@@ -6,7 +6,7 @@ chmod +x ~/coding/grl/OSpkg/morning.app/Contents/MacOS/morning
 codesign --deep --force --verbose --sign "Apple Development: wdboyes@icloud.com (5SFDQ6365C)" ~/coding/grl/OSpkg/morning.app
 
 # windows packaging
-cp -r ~/coding/jdk21-win/* ~/coding/grl/OSpkg/win/jre/
+cp -r ~/coding/jre21-win/* ~/coding/grl/OSpkg/win/jre/
 cp ~/coding/grl/app/build/javafx-libs/*win.jar ~/coding/grl/OSpkg/win/lib/
 cp ~/coding/grl/app/build/libs/app-all.jar ~/coding/grl/OSpkg/win/
 java -jar /Users/william/launch4j/launch4j.jar ~/coding/grl/OSpkg/win/config.xml
